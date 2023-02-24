5c004f0801d77526ea2820fff382643a81704600
imaddine1 <hh.imad2016@gmail.com>
2023-02-24 23:44:40 +0100

    some_upd_to_use_it_after

---
+++ wp-php-fpm/wp-config.php
@@ -0,0 +1,26 @@
+*
+ * * Database settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+
+// ** Database settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'wp_db' );
+
+/** Database username */
+define( 'DB_USER', 'imad' );
+
+/** Database password */
+define( 'DB_PASSWORD', 'imad123' );
+
+/** Database hostname */
+define( 'DB_HOST', 'localhost' );
+
+/** Database charset to use in creating database tables. */
+"wp-config.php" [dos] 96L, 2982B                                             22,45         12%
