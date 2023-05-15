1977149a091e1259cd2bfc5c46ed47e196968357
imaddine1 <hh.imad2016@gmail.com>
2023-05-15 20:28:47 +0100

    some_fix for mac

--- mariadb/db.sh
+++ mariadb/db.sh
@@ -2,6 +2,6 @@ service mariadb start
 mysql -e "CREATE DATABASE IF NOT EXISTS $MY_DATABASE ;"
 mysql -e "CREATE USER IF NOT EXISTS $MY_USER@'%' IDENTIFIED BY '$MY_PASS' ;"
 mysql -e "GRANT ALL PRIVILEGES ON $MY_DATABASE.* TO $MY_USER@'%' ;"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MY_ROOT';"
+#mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MY_ROOT';"
 kill $(cat /var/run/mysqld/mysqld.pid)
 mysqld
--- wordpress/create.sh
+++ wordpress/create.sh
@@ -1,9 +1,10 @@
 service php7.4-fpm start
 service php7.4-fpm stop
-sleep 1
+#sleep 1
+mkdir /var/www/html/wordpress
 cd /var/www/html/wordpress
-# wp core download --allow-root
-#wp core config --dbname=$MY_DATABASE --dbuser=$MY_USER --dbpass=$MY_PASS --dbhost=$MY_HOST --path=/var/www/html/wordpress --allow-root
+wp core download --allow-root
+wp core config --dbname=$MY_DATABASE --dbuser=$MY_USER --dbpass=$MY_PASS --dbhost=$MY_HOST --path=/var/www/html/wordpress --allow-root
 wp core install --url=localhost --title=$BLOG_TITLE --admin_user=$AD_USER --admin_password=$AD_PASS --admin_email=$AD_EMAIL --path=/var/www/html/wordpress  --allow-root
 wp user create $SP_USER $SP_EMAIL --role=subscriber --user_pass=$SP_PASS --path=/var/www/html/wordpress  --allow-root
 
