bca630e6da56f936d44bc0603df477e2d724ecd3
imaddine1 <hh.imad2016@gmail.com>
2023-05-17 16:31:45 +0100

    this_update_use_bind_mount

--- srcs/requirements/wordpress/tools/create.sh
+++ srcs/requirements/wordpress/tools/create.sh
@@ -1,6 +1,13 @@
 service php7.3-fpm start
 service php7.3-fpm stop
-sleep 3
+
+mkdir -p /var/www/html
+cd /var/www/html
+wget https://wordpress.org/latest.tar.gz
+tar xvf latest.tar.gz
+cd /var/www/html
+rm -f latest.tar.gz
+
 wp core config --dbname=$MY_DATABASE --dbuser=$MY_USER --dbpass=$MY_PASS --dbhost=$MY_HOST --path=/var/www/html/wordpress --allow-root
 wp core install --url=localhost --title=$BLOG_TITLE --admin_user=$AD_USER --admin_password=$AD_PASS --admin_email=$AD_EMAIL --path=/var/www/html/wordpress  --allow-root
 wp user create $SP_USER $SP_EMAIL --role=subscriber --user_pass=$SP_PASS --path=/var/www/html/wordpress  --allow-root
