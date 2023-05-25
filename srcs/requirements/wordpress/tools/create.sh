ab2f8ffcfa84da0a056ae9bb9b5e0317c911526f
root <root@iharile.42.fr>
2023-05-25 05:47:15 -0500

    Root_change_locahost

--- srcs/requirements/wordpress/tools/create.sh
+++ srcs/requirements/wordpress/tools/create.sh
@@ -13,7 +13,7 @@ sleep 8;
 cd /var/www/html/wordpress
 #wp core download --allow-root
 wp core config --dbname=$MY_DATABASE --dbuser=$MY_USER --dbpass=$MY_PASS --dbhost=$MY_HOST --path=/var/www/html/wordpress --allow-root
-wp core install --url=localhost --title=$BLOG_TITLE --admin_user=$AD_USER --admin_password=$AD_PASS --admin_email=$AD_EMAIL --path=/var/www/html/wordpress  --allow-root
+wp core install --url=iharile.42.fr --title=$BLOG_TITLE --admin_user=$AD_USER --admin_password=$AD_PASS --admin_email=$AD_EMAIL --path=/var/www/html/wordpress  --allow-root
 wp user create $SP_USER $SP_EMAIL --role=subscriber --user_pass=$SP_PASS --path=/var/www/html/wordpress  --allow-root
 
 php-fpm7.3 -F
